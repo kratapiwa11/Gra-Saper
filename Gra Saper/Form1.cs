2af4f8cc2b19b514a7552749be0ea501cf799530
tupacalive1 <kiliaan0406@gmail.com>
2026-05-20 08:52:20 +0200

    plansza commit

--- Gra Saper/Form1.cs
+++ Gra Saper/Form1.cs
@@ -4,24 +4,30 @@ using System.Windows.Forms;
 
 public class Form1 : Form
 {
-    private const int GridSize = 10;
-    private const int CellSize = 40;
+    private const int GridSize = 10; //TO JEST wielkosc siatki grid czyli 10
+    private const int KolumnaSize = 40; // to jest wielkosc kolmny czli 40
     private Button[,] buttons = new Button[GridSize, GridSize];
     private bool[,] mines = new bool[GridSize, GridSize];
-    private int mineCount = 10;
+    private int liczbamin = 10;
 
     public Form1()
     {
         this.Text = "Saper";
-         this.ClientSize = new Size(GridSize * CellSize, GridSize * CellSize);
+        this.ClientSize = new Size(GridSize * KolumnaSize, GridSize * KolumnaSize);
         InitializeGrid();
-        PlaceMines();
+        liczbamin();
         CalculateNumbers();
     }
-    
+    // x i y pionowo i poziomo
  private void InitializeGrid()
     {
         for (int x = 0; x < GridSize; x++)
         {
             for (int y = 0; y < GridSize; y++)
             {
+                buttons[x, y] = new Button();
+                buttons[x, y].Size = new Size(KolumnaSize, KolumnaSize);
+                buttons[x, y].Location = new Point(x * KolumnaSize, y * KolumnaSize);
+                buttons[x, y].Click += new EventHandler(Button_Click);
+                this.Controls.Add(buttons[x, y]);
+                }
